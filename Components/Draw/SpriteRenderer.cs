e6a1e5596c62b52070627c452b99b0974411cf79
cody bolleboom <cody.bolleboom@hva.nl>
2021-03-15 15:38:22 +0100

    Added SpriteEffects

--- Components/Draw/SpriteRenderer.cs
+++ Components/Draw/SpriteRenderer.cs
@@ -8,6 +8,7 @@ namespace Component
     {
         public Texture2D sprite;
         public float rotation;
+        public SpriteEffects SpriteEffect = SpriteEffects.None; 
 
         public SpriteRenderer(Texture2D spr)
         {
@@ -20,11 +21,18 @@ namespace Component
             rotation = angle;
         }
 
+        public SpriteRenderer(Texture2D spr, float angle = 0, SpriteEffects spriteEffect = SpriteEffects.None)
+        {
+            sprite = spr;
+            rotation = angle;
+            SpriteEffect = spriteEffect;
+        }
+
         public void Draw(SpriteBatch spriteBatch) {
             //spriteBatch.Draw(sprite, gameObject.rectangle, Color.White);
 
             Vector2 origin = new Vector2(sprite.Width / 2, sprite.Height / 2);
-            spriteBatch.Draw(sprite, GameObject.rectangle, null, Color.White, rotation, origin, SpriteEffects.None, 0f);
+            spriteBatch.Draw(sprite, GameObject.rectangle, null, Color.White, rotation, origin, SpriteEffect, 0f);
 
 
         }
