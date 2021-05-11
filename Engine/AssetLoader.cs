50ee8ee4e79097fcae4bcd5d30ad10ea0ee5b60d
cody bolleboom <cody.bolleboom@hva.nl>
2021-05-11 15:39:24 +0200

    added songs

--- Engine/AssetLoader.cs
+++ Engine/AssetLoader.cs
@@ -7,7 +7,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Media;
 
-namespace TestProject
+namespace Engine
 {
     public class AssetLoader
     {
